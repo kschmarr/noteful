6c87a912a7c1a23c1a32c75914febab13421d69b
Kris Schmarr <kschmarr@gmail.com>
2019-07-23 10:46:48 -0600

    removed trailing / from endpoint address

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: "https://agile-sands-26766.herokuapp.com/",
+  API_ENDPOINT: "https://agile-sands-26766.herokuapp.com",
   API_KEY: process.env.REACT_APP_API_KEY
 };
